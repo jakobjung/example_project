25dcc97c053c9b321aeae6e5ea352c0a43734e19
Jakob Jung <jakob@wcvh001.rzhousing.uni-wuerzburg.de>
2021-11-10 13:52:29 +0100

    initial commit

---
+++ scripts/trimm_map.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# map reads:
+~/bin/bbmap/bbmap.sh ref=../data/reference_sequences/genome.fasta\
+		     in=raw_reads_R1.fastq.gz \
+		     out=../data/aligned_reads_R1.bam
+
+#get count table:
+featureCounts in=../data/aligned_reads_R1.bam \
+	      -a ../data/reference_sequences/annotation.gff \
+	      out=../analysis/count_table.tab 
